6de915fe41cc7d0a73c9e5919254b6d9a1a904fa
wjdrhs3473 <110841041+wjd4204@users.noreply.github.com>
2024-04-10 00:24:53 +0900

    initial commit

---
+++ src/test/java/com/webtoon/casting/CastingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webtoon.casting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CastingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
